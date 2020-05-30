1690862c4d8076f03dbac920c442403507457b5a
NeoTRAN <neo_tran@outlook.com>
2020-05-30 01:20:49 -0500

    Update Scripts Program and Props

--- Program.cs
+++ Program.cs
@@ -7,10 +7,11 @@ namespace RetoKike
     {
         static int Menu() {
             Console.WriteLine("\n****** Reto de World Hacking by Tux ******\n");
-            Console.WriteLine("Etapa 1:\n"+
-                               " 1- Adivinar el número\n"+
-                               " 2- Pepe quiere una calculadora\n"+
-                               " 3- Robot\n");
+            Console.WriteLine("\n\nEtapa 1:\n"+
+                               " [1]- Adivinar el número\n"+
+                               " [2]- Pepe quiere una calculadora\n"+
+                               " [3]- Robot\n" +
+                               " [99] - Salir\n");
 
             Console.Write("Option: ");
             return Convert.ToUInt16(Console.ReadLine());;
@@ -18,13 +19,21 @@ namespace RetoKike
 
         static void Main(string[] args)
         {
-            int option = Menu();
-
-            switch(option)
+            try
             {
-                case 1: new GuessNumber().run(); break;
-                case 2: new CalculatorPepes().run(); break;
+                bool infinite = true;
+                while (infinite)
+                {
+                    switch (Menu())
+                    {
+                        case 1: new GuessNumber().run(); break;
+                        case 2: new CalculatorPepes().run(); break;
+                        case 3: new SavingEarth().run(); break;
+                        default: infinite = false; break;
+                    }
+                }
             }
+            catch { Console.WriteLine("Opción no valida"); }
         }
     }
 }
