243ded6a3431a2fc452cd9d7ac919813f7923e43
NeoTRAN <neo_tran@outlook.com>
2020-05-31 00:26:38 -0500

    Se ha agregado un caso extra

--- Program.cs
+++ Program.cs
@@ -7,10 +7,11 @@ namespace RetoKike
     {
         static int Menu() {
             Console.WriteLine("\n****** Reto de World Hacking by Tux ******\n");
-            Console.WriteLine("\n\nEtapa 1:\n"+
-                               " [1]- Adivinar el número\n"+
-                               " [2]- Pepe quiere una calculadora\n"+
-                               " [3]- Robot\n" +
+            Console.WriteLine("\nEtapa 1:\n"+
+                               " [1]  - Tienes que adivinar el número\n"+
+                               " [2]  - La PC adivina el número\n"+
+                               " [3]  - Pepe quiere una calculadora\n"+
+                               " [4]  - Robot\n" +
                                " [99] - Salir\n");
 
             Console.Write("Option: ");
@@ -27,8 +28,9 @@ namespace RetoKike
                     switch (Menu())
                     {
                         case 1: new GuessNumber().run(); break;
-                        case 2: new CalculatorPepes().run(); break;
-                        case 3: new SavingEarth().run(); break;
+                        case 2: new PcGuessNumber().run(); break;
+                        case 3: new CalculatorPepes().run(); break;
+                        case 4: new SavingEarth().run(); break;
                         default: infinite = false; break;
                     }
                 }
